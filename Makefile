2f5a63688384d140180bdaa5376d80f807a1be11
Aba <abarajithan07@gmail.com>
2024-06-20 17:58:01 -0700

    Update to verilog for open source tools

--- Makefile
+++ Makefile
@@ -25,10 +25,10 @@ waveform.vcd: ./obj_dir/V$(MODULE)
 	@echo "### BUILDING SIM ###"
 	make -C obj_dir -f V$(MODULE).mk V$(MODULE)
 
-.stamp.verilate: $(MODULE).sv tb_$(MODULE).cpp tests/*
+.stamp.verilate: $(MODULE).v tb_$(MODULE).cpp tests/*
 	@echo
 	@echo "### VERILATING ###"
-	verilator --trace --x-assign unique --x-initial unique -cc $(MODULE).sv --exe tb_$(MODULE).cpp
+	verilator --trace --x-assign unique --x-initial unique -cc $(MODULE).v --exe tb_$(MODULE).cpp
 	@touch .stamp.verilate
 
 	# cmd = f'{SIM_PATH}verilator --binary -j 0 -O3 --trace --relative-includes --top {self.TB_MODULE} -I../ -F ../sources.txt -CFLAGS -I../ {self.MODULE_DIR}/c/sim.c --Mdir ./'
--- processor.v
+++ processor.v
@@ -3,32 +3,33 @@
 module processor #(
   WIDTH = 32, IMEM_DEPTH=512, DMEM_DEPTH=32, NUM_REGS=32
 )(
-  input  logic clock, reset, insMemEn,
-  input  logic [WIDTH-1:0] insMemDataIn, insMemAddr,
-  output logic [WIDTH-1:0] gp, a7 //verifying
+  input  wire clock, reset, insMemEn,
+  input  wire [WIDTH-1:0] insMemDataIn, insMemAddr,
+  output reg  [WIDTH-1:0] gp, a7 //verifying
 );
-  logic [DMEM_DEPTH-1:0][WIDTH-1:0] dataMemory;
-  logic [IMEM_DEPTH-1:0][WIDTH-1:0] insMemory ;
-  logic [NUM_REGS  -1:0][WIDTH-1:0] registers ;
-  logic [3:0] aluOp;
-  logic [4:0] rs1, rs2, rd, opcode;
-  logic [2:0] funct3;
-  logic [6:0] funct7;
-  logic [WIDTH-1:0] ins, imm, pc, data_1, data_2, regDataIn, src1, src2, aluOut, dataMemOut;
-  logic isArithmetic, isImm, isLoadW, isLoadUI, isStoreW, isBranch, isJAL, isJALR, isMUL, isAUIPC, isBranchC, regWriteEn;
+  reg [WIDTH-1:0] dataMemory [DMEM_DEPTH-1:0];
+  reg [WIDTH-1:0] insMemory  [IMEM_DEPTH-1:0];
+  reg [WIDTH-1:0] registers  [NUM_REGS  -1:0];
+  reg [3:0] aluOp;
+  reg [4:0] rs1, rs2, rd, opcode;
+  reg [2:0] funct3;
+  reg [6:0] funct7;
+  reg [WIDTH-1:0] ins, imm, pc, data_1, data_2, regDataIn, src1, src2, aluOut, dataMemOut;
+  reg isArithmetic, isImm, isLoadW, isLoadUI, isStoreW, isBranch, isJAL, isJALR, isMUL, isAUIPC, isBranchC, regWriteEn;
   
   //PC
-  always_ff @(posedge clock)
+  always @(posedge clock)
     if (reset) pc <= 0;
     else       pc <= (isJAL|isJALR|isBranchC) ? aluOut : (pc + 4);
 
   //Instruction memory    //initial $readmemh("tests/rv32ui-p-lui.dump.dat", insMemory);
-  always_ff @(posedge clock) 
+  always @(posedge clock) 
     if (insMemEn) insMemory[insMemAddr] <= insMemDataIn;
 
-  assign ins = insMemEn ? 32'h13 : insMemory[pc[10:2]];
+  always @* begin
 
-  always_comb begin
+    ins = insMemEn ? 32'h13 : insMemory[pc[10:2]];
+
     //Instruction decoder
     {funct7, rs2, rs1, funct3, rd, opcode} = ins[31:2];
     
@@ -50,24 +51,24 @@ module processor #(
     else if (isBranch)         imm = WIDTH'($signed({ins[31]   , ins[7]    , ins[30:25], ins[11:8] , 1'b0})); //sbImm
     else if (isJAL)            imm = WIDTH'($signed({ins[31]   , ins[19:12], ins[20]   , ins[30:21], 1'b0})); //jImm
     else                       imm = 0;
-  end
 
-  //Read Registers     
-  assign data_1 = (rs1 == 0) ? 0 : registers[rs1];
-  assign data_2 = (rs2 == 0) ? 0 : registers[rs2];
-  
-  //Branch decision
-  always_comb case (funct3[2:1])
-    2'b00  : isBranchC = isBranch & (funct3[0] ^ (data_1 == data_2));                  //BNE, BEQ 
-    2'b10  : isBranchC = isBranch & (funct3[0] ^ ($signed(data_1) < $signed(data_2))); //BLT, BGE
-    2'b11  : isBranchC = isBranch & (funct3[0] ^ (data_1 < data_2));                   //BLTU, BGEU
-    default: isBranchC = 1'b0;
-  endcase
+    //Read Registers     
+    data_1 = (rs1 == 0) ? 0 : registers[rs1];
+    data_2 = (rs2 == 0) ? 0 : registers[rs2];
+
+    //Branch decision
+    case (funct3[2:1])
+      2'b00  : isBranchC = isBranch & (funct3[0] ^ (data_1 == data_2));                  //BNE, BEQ 
+      2'b10  : isBranchC = isBranch & (funct3[0] ^ ($signed(data_1) < $signed(data_2))); //BLT, BGE
+      2'b11  : isBranchC = isBranch & (funct3[0] ^ (data_1 < data_2));                   //BLTU, BGEU
+      default: isBranchC = 1'b0;
+    endcase
+  end  
 
   //ALU
   localparam [3:0] ADD=0, SLL=1, SLT=2, SLTU=3, XOR=4, SRL=5, OR=6, AND=7, SUB=8, MUL=9, DIV=10, SRA=13, PASS=15;
 
-  always_comb begin
+  always @* begin
     if      (isMUL)                                          aluOp = (funct3[2] ? DIV : MUL);
     else if (isArithmetic)                                   aluOp = {funct7[5]                   , funct3};
     else if (isImm)                                          aluOp = {funct7[5] & (funct3==3'b101), funct3};
@@ -77,7 +78,7 @@ module processor #(
     src1 = (isJAL|isBranch|isAUIPC)                                        ? pc  : data_1;
     src2 = (isImm|isLoadW|isLoadUI|isJAL|isJALR|isStoreW|isBranch|isAUIPC) ? imm : data_2;
 
-    unique case (aluOp)
+    case (aluOp)
       ADD    : aluOut = src1 + src2;
       SUB    : aluOut = src1 - src2;                                      
       SLL    : aluOut = src1 << src2[4:0];                                
@@ -93,18 +94,19 @@ module processor #(
       PASS   : aluOut = src2;                                             
       default: aluOut = 0;
     endcase 
+
+    {gp, a7, dataMemOut} = {registers[3], registers[17], dataMemory[aluOut]}; //For verification
+
+    // Writeback to register bank
+    regWriteEn = isArithmetic|isImm|isLoadW|isLoadUI|isJAL|isJALR|isAUIPC;
+    regDataIn  = (isJALR|isJAL) ? (pc + 4) : (isLoadW ? dataMemOut : aluOut); //Writeback Mux
   end
 
   //Data memory    //initial $readmemh("data/data.dat",dataMemory);
-  always_ff @(posedge clock) 
+  always @(posedge clock) 
     if (isStoreW) dataMemory[aluOut] <= data_2;
 
-  // Writeback to register bank
-  assign regWriteEn = isArithmetic|isImm|isLoadW|isLoadUI|isJAL|isJALR|isAUIPC;
-  assign regDataIn  = (isJALR|isJAL) ? (pc + 4) : (isLoadW ? dataMemOut : aluOut); //Writeback Mux
-
-  always_ff @(posedge clock) //initial $readmemh("data/registry.dat", registers);
+  always @(posedge clock) //initial $readmemh("data/registry.dat", registers);
     if (regWriteEn) registers[rd] <= regDataIn;
 
-  assign {gp, a7, dataMemOut} = {registers[3], registers[17], dataMemory[aluOut]}; //For verification
 endmodule
